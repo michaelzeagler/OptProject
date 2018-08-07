e6cec1c3add83728da23105013971d2d8767f1d3
Mike <michael.zeagler@gmail.com>
2018-08-07 03:01:15 -0500

    Made some formatting changes.

--- u_io.py
+++ u_io.py
@@ -1,17 +1,18 @@
 # Feedback linearizing control law for a relative degree 2 system
-def u_IO(xst, ydes, yddes, f, g, h_x, hd_x, dLie, poles):
+def u_IO(xst, ydes, yddes, f_x, g_x, h_x, hd_x, dLie, poles):
     
+    # computing Lie derivatives
+    l2fh  = dLie*f_x
+    lglfh = dLie*g_x
     
-    l2fh  = dLie*f
-    lglfh = dLie*g
-
+    # output errors 
     e_y = h_x  - ydes
     e_yd =hd_x - yddes
     
-    
+    # constrolled system dynamics definition
     nu   = e_y.multiply_elementwise(poles[:,0]) + \
         e_yd.multiply_elementwise(poles[:,1])
-
+    
     u = inverse(lglfh)*(l2fh - nu)
 
     return u
